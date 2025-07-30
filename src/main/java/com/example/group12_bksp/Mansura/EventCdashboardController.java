e05bcc0f190d0b48c707a8471dfce3afa8bf5ce1
man-sura <2220374@iub.edu.bd>
2025-07-30 12:28:11 +0600

    added scene on EventCdashbord.fxml and update controller

--- src/main/java/com/example/group12_bksp/Mansura/EventCdashboardController.java
+++ src/main/java/com/example/group12_bksp/Mansura/EventCdashboardController.java
@@ -1,7 +1,14 @@
 package com.example.group12_bksp.Mansura;
 
+import javafx.event.ActionEvent;
+
 public class EventCdashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void schedulepracticebuttton(ActionEvent actionEvent) {
+    }
+}
