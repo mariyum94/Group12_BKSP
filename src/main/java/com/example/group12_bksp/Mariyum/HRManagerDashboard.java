0495782fb4b7a626e2a52f138483c2908b47177e
mariyum <mariyambintealkader@gmail.com>
2025-07-30 10:38:32 +0600

    create scene on "HRManagerDashboard" and also update controller class, based on the MS1-8 goals as a button

--- src/main/java/com/example/group12_bksp/Mariyum/HRManagerDashboard.java
+++ src/main/java/com/example/group12_bksp/Mariyum/HRManagerDashboard.java
@@ -1,7 +1,46 @@
 package com.example.group12_bksp.Mariyum;
 
+import javafx.event.ActionEvent;
+
 public class HRManagerDashboard
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void ManageAthleteStaffRecordOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void NotifyEmployeeOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RecruitNewStafforTrainerOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void EvaluatePerformanceReportOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void TrackAttendanceOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GenerateHRReportOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LogOutOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ManageHealthFitnessRecordOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ScheduleTrainingSessionOnActionButton(ActionEvent actionEvent) {
+    }
+}
