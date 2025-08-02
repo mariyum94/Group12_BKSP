d2667ab8b776dd367c2a4ea4609dc3637766aa20
man-sura <2220374@iub.edu.bd>
2025-08-02 14:27:30 +0600

    added scene on EventC4.fxml and update controller a

---
+++ src/main/java/com/example/group12_bksp/Mansura/EventC4.java
@@ -0,0 +1,35 @@
+package com.example.group12_bksp.Mansura;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class EventC4
+{
+    @javafx.fxml.FXML
+    private TableView participantstable;
+    @javafx.fxml.FXML
+    private TableColumn nameColumn;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TableColumn idColumn;
+    @javafx.fxml.FXML
+    private TableColumn emailColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backToDashboard(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeParticipant(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approveParticipant(ActionEvent actionEvent) {
+    }
+}
