c28267f7f7e6dac6b96941f4f19d45d868bc79f1
mariyum <mariyambintealkader@gmail.com>
2025-08-03 20:46:33 +0600

    create scene design on "FinanceOfficer2"  and update its controller class but doesn't implement yet

--- src/main/java/com/example/group12_bksp/Mariyum/FinanceOfficer2.java
+++ src/main/java/com/example/group12_bksp/Mariyum/FinanceOfficer2.java
@@ -1,7 +1,35 @@
 package com.example.group12_bksp.Mariyum;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class FinanceOfficer2
 {
     @javafx.fxml.FXML
+    private TableColumn salaryCol;
+    @javafx.fxml.FXML
+    private TableColumn nameCol;
+    @javafx.fxml.FXML
+    private TextField SalaryField;
+    @javafx.fxml.FXML
+    private Label reportLabel;
+    @javafx.fxml.FXML
+    private TableView salaryTable;
+    @javafx.fxml.FXML
+    private TextField employenameField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void returnToDashboardButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void paySalaryButton(ActionEvent actionEvent) {
+    }
+}
