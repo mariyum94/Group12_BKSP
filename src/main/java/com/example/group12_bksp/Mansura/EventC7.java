ba29346dac1dc6b51139e6b574b224508d7a7e9f
man-sura <2220374@iub.edu.bd>
2025-08-03 14:34:57 +0600

    added scene on EventC6.fxml and update controller added scene on EventC7.fxml and update controller added scene on EventC8.fxml and update controller

---
+++ src/main/java/com/example/group12_bksp/Mansura/EventC7.java
@@ -0,0 +1,28 @@
+package com.example.group12_bksp.Mansura;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class EventC7
+{
+    @javafx.fxml.FXML
+    private TableView volunteerTable;
+    @javafx.fxml.FXML
+    private TableColumn nameColumn;
+    @javafx.fxml.FXML
+    private TableColumn roleColumn;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TableColumn idColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAssignButton(ActionEvent actionEvent) {
+    }
+}
