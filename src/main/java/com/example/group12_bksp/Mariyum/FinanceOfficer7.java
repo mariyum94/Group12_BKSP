2f5516f2a765d2bfb3ba7290964e75a09bc5f83b
mariyum <mariyambintealkader@gmail.com>
2025-08-04 16:46:42 +0600

    create "FinanceOfficer6" fxml design  and its controller class

--- src/main/java/com/example/group12_bksp/Mariyum/FinanceOfficer7.java
+++ src/main/java/com/example/group12_bksp/Mariyum/FinanceOfficer7.java
@@ -1,7 +1,47 @@
 package com.example.group12_bksp.Mariyum;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class FinanceOfficer7
 {
     @javafx.fxml.FXML
+    private TextField SponsorNameField;
+    @javafx.fxml.FXML
+    private TableView sponsorshipTable;
+    @javafx.fxml.FXML
+    private TableColumn nameColumn;
+    @javafx.fxml.FXML
+    private TableColumn amountColumn;
+    @javafx.fxml.FXML
+    private TableColumn purposeColumn;
+    @javafx.fxml.FXML
+    private TextField PurposeField;
+    @javafx.fxml.FXML
+    private Label messageLabel;
+    @javafx.fxml.FXML
+    private TextField amountField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void editButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void returnToDashboardButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void monitorButton(ActionEvent actionEvent) {
+    }
+}
