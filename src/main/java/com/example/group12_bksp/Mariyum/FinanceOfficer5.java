5d2ff2400cfb02f68cbf088ec9c556d30cbf82e1
mariyum <mariyambintealkader@gmail.com>
2025-08-04 15:35:59 +0600

    add "FinanceOfficer5" fxml and its controller

---
+++ src/main/java/com/example/group12_bksp/Mariyum/FinanceOfficer5.java
@@ -0,0 +1,49 @@
+package com.example.group12_bksp.Mariyum;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class FinanceOfficer5
+{
+    @javafx.fxml.FXML
+    private TableColumn costColumn;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TextField CostField;
+    @javafx.fxml.FXML
+    private TableColumn itemColumn;
+    @javafx.fxml.FXML
+    private TableColumn QuantityColumn;
+    @javafx.fxml.FXML
+    private TableView equipmentTable;
+    @javafx.fxml.FXML
+    private TextField itemField;
+    @javafx.fxml.FXML
+    private TextField quantityField;
+    @javafx.fxml.FXML
+    private Label messageLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void returnToDashboardButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void rejectButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approveButton(ActionEvent actionEvent) {
+    }
+}
