f6c5f26166bd7c2a3604e2abf83cef4b98fc6065
Romtahena Ruhi <romtahenaruhi@gmail.com>
2025-08-05 17:36:42 +0600

    create scene on Cricket Coach1

---
+++ src/main/java/com/example/group12_bksp/ROMTAHENA/CricketCoach1.java
@@ -0,0 +1,33 @@
+package com.example.group12_bksp.ROMTAHENA;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CricketCoach1 {
+
+    @FXML
+    private TextField ageField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField roleField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+
+    @FXML
+    public void submitButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void returnHomeButton(ActionEvent event) {
+
+    }
+
+}
