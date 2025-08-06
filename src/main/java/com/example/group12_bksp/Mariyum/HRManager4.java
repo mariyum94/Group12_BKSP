ccb0ceafa61bbf34adfc371c34e48902c0e543fd
mariyum <mariyambintealkader@gmail.com>
2025-08-07 01:39:01 +0600

    create scene in "HRManager4" also update the controller class

--- src/main/java/com/example/group12_bksp/Mariyum/HRManager4.java
+++ src/main/java/com/example/group12_bksp/Mariyum/HRManager4.java
@@ -1,7 +1,51 @@
 package com.example.group12_bksp.Mariyum;
 
-public class HRManager4
-{
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class HRManager4 {
+
+    @FXML
+    private TableColumn<?, ?> coachColumn;
+
+    @FXML
+    private ComboBox<?> coachComboBox;
+
+    @FXML
+    private TableColumn<?, ?> dateColumn;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    private TableView<?> scheduleTableView;
+
+    @FXML
+    private TableColumn<?, ?> sportColumn;
+
+    @FXML
+    private ComboBox<?> sportComboBox;
+
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @FXML
+    void addScheduleButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnToDashboardButton(ActionEvent event) {
+
+    }
+
+}
