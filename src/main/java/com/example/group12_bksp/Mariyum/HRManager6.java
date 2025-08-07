69abd3c4745b25e5ef6c741ad6b1f6e69d70c502
mariyum <mariyambintealkader@gmail.com>
2025-08-07 21:13:59 +0600

    create scene in "HRManager6" also update the controller class

--- src/main/java/com/example/group12_bksp/Mariyum/HRManager6.java
+++ src/main/java/com/example/group12_bksp/Mariyum/HRManager6.java
@@ -1,7 +1,56 @@
 package com.example.group12_bksp.Mariyum;
 
-public class HRManager6
-{
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class HRManager6 {
+
+    @FXML
+    private TextField NameTF;
+
+    @FXML
+    private TextField RoleTF;
+
+    @FXML
+    private TableColumn<?, ?> nameColumn;
+
+    @FXML
+    private TextField perfomanceTFTF;
+
+    @FXML
+    private TableView<?> performanceTable;
+
+    @FXML
+    private TextArea remarksArea;
+
+    @FXML
+    private TableColumn<?, ?> roleColumn;
+
+    @FXML
+    private TableColumn<?, ?> scoreColumn;
+
+
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @FXML
+    void evaluateButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnToDashboardButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveButton(ActionEvent event) {
+
+    }
+
+}
