bcf56887236331cd3dac1fa7c8a7ee0ba25c3e9f
Samayra Ahmed <2210155@iub.edu.bd>
2025-08-07 20:51:01 +0600

    added scene  design on Swimming Trainee 2.fxml        added scene  design on Swimming Trainee 3.fxml     added scene  design on Swimming Trainee 4.fxml     added java class on SwimmingTrainee2.java          added java class on SwimmingTrainee3.java          added java class on SwimmingTrainee4.java

---
+++ src/main/java/com/example/group12_bksp/Samayra/SwimmingTrainee3.java
@@ -0,0 +1,22 @@
+package com.example.group12_bksp.Samayra;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+public class SwimmingTrainee3
+{
+    @javafx.fxml.FXML
+    private TextArea FeedbackTextArea;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void ReturnHomeButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SaveProgressButtonOnAction(ActionEvent actionEvent) {
+    }
+}
