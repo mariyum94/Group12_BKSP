6757a3b0520d41a11db78039c827afaee3b36e2e
Samayra Ahmed <2210155@iub.edu.bd>
2025-08-08 00:02:18 +0600

    added scene  design on Swimming Trainee 6.fxml

---
+++ src/main/java/com/example/group12_bksp/Samayra/SwimmingTrainee6.java
@@ -0,0 +1,25 @@
+package com.example.group12_bksp.Samayra;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SwimmingTrainee6
+{
+    @javafx.fxml.FXML
+    private TextField HoldTimeTextField;
+    @javafx.fxml.FXML
+    private Label BreathControlOutputL;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitHoldTimeButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ReturnHomeOnActionButton(ActionEvent actionEvent) {
+    }
+}
