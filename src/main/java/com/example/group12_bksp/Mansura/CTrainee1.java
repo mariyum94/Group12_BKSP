aea5a6bde9a82058c1c24e93b98b1432568ac8d7
man-sura <2220374@iub.edu.bd>
2025-08-09 22:48:29 +0600

    added scene on EventC6.fxml and update controller added scene on EventC7.fxml and update controller added scene on EventC8.fxml and update controller

--- src/main/java/com/example/group12_bksp/Mansura/CTrainee1.java
+++ src/main/java/com/example/group12_bksp/Mansura/CTrainee1.java
@@ -1,4 +1,20 @@
 package com.example.group12_bksp.Mansura;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CTrainee1 {
+    @javafx.fxml.FXML
+    private TableColumn timeColumn;
+    @javafx.fxml.FXML
+    private TableColumn locationColumn;
+    @javafx.fxml.FXML
+    private TableView scheduleTable;
+    @javafx.fxml.FXML
+    private TableColumn dateColumn;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TableColumn activityColumn;
 }
