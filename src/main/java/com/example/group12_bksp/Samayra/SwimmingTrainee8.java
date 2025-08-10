c948546e68f475e7707ba78a51531394068b5ec1
Samayra Ahmed <2210155@iub.edu.bd>
2025-08-10 12:50:54 +0600

    added scene  design on Swimming Trainee 8.fxml and changes on the controller class SwimmingTrainee8.java

---
+++ src/main/java/com/example/group12_bksp/Samayra/SwimmingTrainee8.java
@@ -0,0 +1,35 @@
+package com.example.group12_bksp.Samayra;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SwimmingTrainee8
+{
+    @javafx.fxml.FXML
+    private TextField EventNameTextField;
+    @javafx.fxml.FXML
+    private TextField RaceTimeTextField;
+    @javafx.fxml.FXML
+    private Label St8OutputLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void UploadRaceVidOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AnalyzeRacePerformanceButton(ActionEvent actionEvent) {
+    }
+
+    @java.lang.Deprecated
+    public void ReturnHomeOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ReturnHomeOnAction(ActionEvent actionEvent) {
+    }
+}
