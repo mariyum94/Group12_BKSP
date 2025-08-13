14460479cc6f4a274446ba63d733fdfa97f8e6e3
man-sura <2220374@iub.edu.bd>
2025-08-13 15:47:39 +0600

    added scene on CTrainee1.fxml and update controller added scene on CTrainee2.fxml and update controller added scene on CTrainee3.fxml and update controller

--- src/main/java/com/example/group12_bksp/Mansura/CTrainee1.java
+++ src/main/java/com/example/group12_bksp/Mansura/CTrainee1.java
@@ -1,20 +1,29 @@
 package com.example.group12_bksp.Mansura;
 
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-public class CTrainee1 {
+public class CTrainee1
+{
     @javafx.fxml.FXML
     private TableColumn timeColumn;
     @javafx.fxml.FXML
+    private TableColumn sessionTypeColumn;
+    @javafx.fxml.FXML
     private TableColumn locationColumn;
+    @javafx.fxml.FXML
+    private TableColumn coachColumn;
     @javafx.fxml.FXML
     private TableView scheduleTable;
     @javafx.fxml.FXML
     private TableColumn dateColumn;
+
     @javafx.fxml.FXML
-    private Label statusLabel;
+    public void initialize() {
+    }
+
     @javafx.fxml.FXML
-    private TableColumn activityColumn;
+    public void handleMySchedule(ActionEvent actionEvent) {
+    }
 }
---
+++ src/main/java/com/example/group12_bksp/Mansura/CTrainee3.java
@@ -0,0 +1,23 @@
+package com.example.group12_bksp.Mansura;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class CTrainee3
+{
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TableColumn dateColumn;
+    @javafx.fxml.FXML
+    private TableView attendanceTable;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLoadAttendance(ActionEvent actionEvent) {
+    }
+}
