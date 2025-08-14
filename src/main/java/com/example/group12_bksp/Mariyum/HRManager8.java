81c5d181ac1a50036265c939a8f9be4a08a9c58f
mariyum <mariyambintealkader@gmail.com>
2025-08-14 10:47:10 +0600

    update "HRManager8" also update the model class "HRManagerModelClass5"

--- src/main/java/com/example/group12_bksp/Mariyum/HRManager8.java
+++ src/main/java/com/example/group12_bksp/Mariyum/HRManager8.java
@@ -6,22 +6,24 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class HRManager8 {
 
     @FXML
-    private TableView<?> healthTable;
+    private TableView<HRManagerModelClass5> healthTable;
 
     @FXML
-    private TableColumn<?, ?> injuryCol;
+    private TableColumn<HRManagerModelClass5, String> injuryCol;
 
     @FXML
     private TextField injuryField;
 
     @FXML
-    private TableColumn<?, ?> nameCol;
+    private TableColumn<HRManagerModelClass5, String> nameCol;
 
     @FXML
     private TextField nameField;
@@ -30,19 +32,41 @@ public class HRManager8 {
     private TextField textarea;
 
     @FXML
-    private TableColumn<?, ?> weightCol;
+    private TableColumn<HRManagerModelClass5, Double> weightCol;
 
     @FXML
     private TextField weightField;
 
+    static ArrayList<HRManagerModelClass5> HRManagerModelClass5list= new ArrayList<>();
 
     @javafx.fxml.FXML
     public void initialize() {
-    }
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        injuryCol.setCellValueFactory(new PropertyValueFactory<>("injury"));
+        weightCol.setCellValueFactory(new PropertyValueFactory<>("weight"));
+
+        healthTable.getItems().addAll(HRManagerModelClass5list);}
+
     @FXML
     void addRecordButton(ActionEvent event) {
+        String name = nameField.getText().trim();
+        String injury = injuryField.getText().trim();
+        String weightText = weightField.getText().trim();
 
+        if (name.isEmpty() || injury.isEmpty() || weightText.isEmpty()) {
+            return;
+        }
+
+        double weight = Double.parseDouble(weightText);//weightField.getText().trim().
+        HRManagerModelClass5 record = new HRManagerModelClass5(name, weight, injury);
+        HRManagerModelClass5list.add(record);
+        healthTable.getItems().add(record);
+
+        nameField.clear();
+        weightField.clear();
+        injuryField.clear();
     }
+
 
     @FXML
     void returnToDashboardButton(ActionEvent actionEvent) throws IOException {
