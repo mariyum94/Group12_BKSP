073209b41911bd4d34308cc0a9d8531854cde09d
Samayra Ahmed <2210155@iub.edu.bd>
2025-08-15 18:19:20 +0600

    updated scene designs of Swimming Trainee 3.fxml Swimming Trainee 4.fxml and controller of SwimmingTrainee1.java SwimmingTrainee2.java SwimmingTrainee3.java SwimmingTrainee4.java and model class of SwimmingTraineeModelClass1.java SwimmingTraineeModelClass2.java SwimmingTraineeModelClass3.java SwimmingTraineeModelClass4.java

--- src/main/java/com/example/group12_bksp/Samayra/SwimmingTrainee2.java
+++ src/main/java/com/example/group12_bksp/Samayra/SwimmingTrainee2.java
@@ -1,9 +1,13 @@
 package com.example.group12_bksp.Samayra;
 
+import com.example.group12_bksp.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 public class SwimmingTrainee2
 {
@@ -22,9 +26,24 @@ public class SwimmingTrainee2
 
     @javafx.fxml.FXML
     public void SaveTrainingOnAction(ActionEvent actionEvent) {
+        String LapTime= LapTimeTextField.getText();
+        String RestDuration= RestDurationTextField.getText();
+        LocalDate sessionDate= SessionDatePicker.getValue();
+
+        if (sessionDate == null || LapTime.isEmpty() || RestDuration.isEmpty()) {
+            St2OutputLabel.setText("Please complete all fields before saving.");
+            return;
+        }
+
+
+        St2OutputLabel.setText("Your session has been saved successfully for" + sessionDate );
+
+
+
     }
 
     @javafx.fxml.FXML
-    public void ReturnHomeOnAction(ActionEvent actionEvent) {
+    public void ReturnHomeOnAction(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/group12_bksp/Samayra/SwimmingTraineeDashboardController.fxml", actionEvent);
     }
 }
