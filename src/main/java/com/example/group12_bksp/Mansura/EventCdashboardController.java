e4e323db3f160e22d7e08c8bf55ef5b6aa6de947
man-sura <2220374@iub.edu.bd>
2025-08-15 23:05:04 +0600

    update EventCdashboardController.java Controller class

--- src/main/java/com/example/group12_bksp/Mansura/EventCdashboardController.java
+++ src/main/java/com/example/group12_bksp/Mansura/EventCdashboardController.java
@@ -12,52 +12,47 @@ public class EventCdashboardController {
     }
 
     @javafx.fxml.FXML
-    public void schedulepracticebuttton(ActionEvent actionEvent) throws IOException {
-        SceneSwitcher.switchTo("schedulePractice.fxml", actionEvent);
-    }
-
-    @javafx.fxml.FXML
     public void assignvolunteerbutton(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchTo("assignVolunteer.fxml", actionEvent);
-    }
+    }//EventC7
 
     @javafx.fxml.FXML
     public void manageparticipantsbutton(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchTo("manageParticipants.fxml", actionEvent);
-    }
+    }//EventC4
 
     @javafx.fxml.FXML
     public void viewfeedbackbutton(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchTo("viewFeedback.fxml", actionEvent);
-    }
+    }//EventC8
 
     @javafx.fxml.FXML
     public void editeventbutton(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchTo("editEvent.fxml", actionEvent);
-    }
+    }//Editevent
 
     @javafx.fxml.FXML
     public void postannoucementbutton(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchTo("postAnnouncement.fxml", actionEvent);
-    }
+    }//EventC6
 
     @javafx.fxml.FXML
     public void uploaddocumentsbutton(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchTo("uploadDocuments.fxml", actionEvent);
-    }
+    }//EventC5
 
     @javafx.fxml.FXML
     public void vieweventbutton(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchTo("viewEvent.fxml", actionEvent);
-    }
+    }//Viewevent
 
     @javafx.fxml.FXML
     public void logoutbutton(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchTo("login.fxml", actionEvent);
-    }
+    }//
 
     @javafx.fxml.FXML
     public void createeventbuttton(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchTo("createEvent.fxml", actionEvent);
-    }
+    }//Createevent
 }
