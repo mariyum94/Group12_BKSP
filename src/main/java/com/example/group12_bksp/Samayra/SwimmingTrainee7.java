d2d46e9882c42a5412a1bc7ea9cb9a296f5cd2ca
Samayra Ahmed <2210155@iub.edu.bd>
2025-08-15 22:20:02 +0600

    updated scene design Swimming Trainee 7.fxml

--- src/main/java/com/example/group12_bksp/Samayra/SwimmingTrainee7.java
+++ src/main/java/com/example/group12_bksp/Samayra/SwimmingTrainee7.java
@@ -1,16 +1,17 @@
 package com.example.group12_bksp.Samayra;
 
+import com.example.group12_bksp.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+
 public class SwimmingTrainee7
 {
     @javafx.fxml.FXML
     private TextField TreadTimeTextField;
-    @javafx.fxml.FXML
-    private ComboBox DifficultyComboBox7;
     @javafx.fxml.FXML
     private Label St7OutputLabel;
 
@@ -18,19 +19,26 @@ public class SwimmingTrainee7
     public void initialize() {
     }
 
-    @java.lang.Deprecated
-    public void ReturnHomeButtonOnAction(ActionEvent actionEvent) {
-    }
-
-    @java.lang.Deprecated
-    public void LogResultButtonOnAction(ActionEvent actionEvent) {
-    }
 
     @javafx.fxml.FXML
-    public void ReturnHomeOnAction(ActionEvent actionEvent) {
+    public void ReturnHomeOnAction(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/group12_bksp/Samayra/SwimmingTraineeDashboardController.fxml", actionEvent);
     }
 
     @javafx.fxml.FXML
     public void SubmitTimeButtonOnAction(ActionEvent actionEvent) {
+        String timeTaken= TreadTimeTextField.getText();
+
+        if (timeTaken.isEmpty()){
+
+            St7OutputLabel.setText("Please input your time taken");
+            return;
+        }
+        int Timetaken=Integer.parseInt(timeTaken);
+        if (Timetaken<5){
+            St7OutputLabel.setText("Sorry participate again later to record 5 minutes");
+        }else{
+            St7OutputLabel.setText("Congratulations!Your new record has been stored successfully");
+        }
     }
 }
--- src/main/java/com/example/group12_bksp/Samayra/SwimmingTrainee8.java
+++ src/main/java/com/example/group12_bksp/Samayra/SwimmingTrainee8.java
@@ -1,36 +1,42 @@
 package com.example.group12_bksp.Samayra;
 
+import com.example.group12_bksp.SceneSwitcher;
 import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class SwimmingTrainee8
 {
     @javafx.fxml.FXML
     private Label St8OutputLabel;
     @javafx.fxml.FXML
-    private TextField timingTextField;
-    @javafx.fxml.FXML
-    private TableView tableview8;
+    private ComboBox<String> SwimMeetCB;
 
     @javafx.fxml.FXML
     public void initialize() {
+        SwimMeetCB.getItems().addAll("Olympics", "Regional Championship", "Nationals");
+    }
+
+
+    @javafx.fxml.FXML
+    public void ReturnHomeOnAction(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/group12_bksp/Samayra/SwimmingTraineeDashboardController.fxml", actionEvent);
+
     }
 
     @javafx.fxml.FXML
-    public void UploadRaceVidOnAction(ActionEvent actionEvent) {
+    public void ApplyMeetButton(ActionEvent actionEvent) {
+        String MeetCombo= SwimMeetCB.getValue();
+
+        if (MeetCombo==null){
+            St8OutputLabel.setText("Please select Meet type");
+            return;
+        }
+        St8OutputLabel.setText("Application Submitted You Will be contacted soon");
     }
 
-    @javafx.fxml.FXML
-    public void AnalyzeRacePerformanceButton(ActionEvent actionEvent) {
-    }
-
-    @java.lang.Deprecated
-    public void ReturnHomeOnActionButton(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void ReturnHomeOnAction(ActionEvent actionEvent) {
-    }
 }
